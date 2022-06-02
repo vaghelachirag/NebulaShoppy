fb2f3f9301b679ce8b9764c009b9849770560722
vaghelachirag <chirag.v@nebulacompanies.com>
2022-06-02 18:36:42 +0530

    working account

---
+++ lib/model/setmyAccount/setmyAccount.dart
@@ -0,0 +1,12 @@
+class SetMyAccount {
+  final int postition;
+  final int Title;
+  bool is_Ewallet;
+ 
+
+  SetMyAccount(
+      {required this.postition,
+      required this.Title,
+      required this.is_Ewallet,
+     });
+}
