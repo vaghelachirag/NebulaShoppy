3e62e64ccc54a253de9b640bfa56c2c0a2b6eec9
vaghelachirag <chirag.v@nebulacompanies.com>
2022-06-09 17:40:44 +0530

    added code for skeleton loder

--- lib/screen/test.dart
+++ lib/screen/test.dart
@@ -9,6 +9,7 @@ import '../model/homescreen/itemNewLaunched.dart';
 import '../model/homescreen/itemhomecategory.dart';
 import '../model/product.dart';
 import '../network/service.dart';
+import '../uttils/skeletonloader.dart';
 import '../widget/categoryproductWidget.dart';
 import '../widget/soldoutdialoug.dart';
 import '../widget/timelineComponent.dart';
@@ -30,6 +31,8 @@ import '../model/search/SearchProduct.dart';
 import 'dart:convert';
 import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Test extends StatefulWidget {
   @override
@@ -41,52 +44,88 @@ class _TestState extends State<Test> {
   @override
   void initState() {
     super.initState();
-    getProductDetailImage();
+
   }
 
   @override
   Widget build(BuildContext context) {
-   return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Timeline',
-      theme: ThemeData(
-        primaryColor: kPurpleColor,
+   return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text("Skeleton Loading with shimmer", style: TextStyle(color: Colors.black),),
       ),
-      home: TimelineComponet(ticks: 1),
+      body: Padding(
+        padding: const EdgeInsets.all(0.0),
+        child:  Container(
+       width: MediaQuery.of(context).size.width/2,
+      child: Flexible(
+          child: GridView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext ctx, index) {
+            int timer = 2000;
+            return Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade300,
+              period: Duration(milliseconds: timer),
+              child: boxProductCatWise(context),
+            );
+          },
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 1,
+          childAspectRatio: 8.0 / 12.0,
+        )),
+      ),
+     
+    )));
+  }
+
+
+  Widget box(){
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15)
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.grey
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(5),
+                    width: 100,
+                    height: 10,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.grey),
+                  ),
+                   Container(
+            margin: EdgeInsets.all(5),
+                    width: 80,
+                    height: 8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.grey),
+                  ),
+                   Container(
+            margin: EdgeInsets.all(5),
+                    width: 60,
+                    height: 5,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.grey),
+                  )
+                  
+        ],
+      ),
     );
   }
-
-  void getProductDetailImage() async {
-    final response = await http.get(Uri.parse(BASE_URL +
-        WS_GET_PRODUCT_DETAILS_IMAGE +
-        "?" +
-        "EComProductDetailsId=" +
-        "31"));
-    dynamic myjson = json.decode(response.body.toString());
-    List<dynamic> imagedata = myjson["Data"];
-
-    print("ResponseCode" + imagedata.toString());
-  }
-
-  void showNoProduct() {
-    showDialog(
-              barrierColor: Colors.black26,
-              context: context,
-              builder: (context) {
-                return SoldOutDialoug(
-                  title: "Soldout",
-                  description: "This product may not be available at the selected address.",
-                );
-              },
-            );
-  }
 }
-
-final colors = const [
-  Colors.red,
-  Colors.green,
-  Colors.greenAccent,
-  Colors.amberAccent,
-  Colors.blue,
-  Colors.amber,
-];
